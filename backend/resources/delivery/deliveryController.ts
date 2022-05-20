ecb7362b3a297af0c79f15dbcab27fd50c0d313e
Jannie87 <jannie.backman@gmail.com>
2022-05-20 11:41:46 +0200

    delivery setup

---
+++ backend/resources/delivery/deliveryController.ts
@@ -0,0 +1,11 @@
+import { NextFunction, Request, Response } from "express";
+
+export const getDelivery = async (req: Request, res: Response) => {};
+
+export const registerDelivery = async (req: Request, res: Response) => {};
+
+export const getDeliveries = async (req: Request, res: Response) => {};
+
+export const updateDelivery = async (req: Request, res: Response) => {};
+
+export const deleteDelivery = async (req: Request, res: Response) => {};
