251bbced0b8a09b20631920d3c612d6f647605d3
Jannie87 <jannie.backman@gmail.com>
2022-05-20 12:40:15 +0200

    added delivery controller

--- backend/resources/delivery/deliveryController.ts
+++ backend/resources/delivery/deliveryController.ts
@@ -1,11 +1,34 @@
 import { NextFunction, Request, Response } from "express";
+import { nextTick } from "process";
+import { DeliveryModel } from "./deliveryModel";
+
+export const getDeliveries = async (req: Request, res: Response) => {
+  const deliveries = await DeliveryModel.find();
+  res.status(200).json(deliveries);
+};
 
 export const getDelivery = async (req: Request, res: Response) => {};
 
-export const registerDelivery = async (req: Request, res: Response) => {};
+export const registerDelivery = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const delivery = new DeliveryModel(req.body);
+    await delivery.save();
+    res.status(200).json(delivery);
+  } catch (err) {
+    next(err);
+  }
+};
 
-export const getDeliveries = async (req: Request, res: Response) => {};
+export const updateDelivery = async (req: Request, res: Response) => {
+  const delivery = await DeliveryModel.findById(req.params.id);
+  console.log(delivery);
+  res.status(200).json(delivery);
+};
 
-export const updateDelivery = async (req: Request, res: Response) => {};
-
-export const deleteDelivery = async (req: Request, res: Response) => {};
+export const deleteDelivery = async (req: Request, res: Response) => {
+  res.status(200).json("Delivery deleted");
+};
