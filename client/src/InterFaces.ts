c561acb6c58160b7aad08f4e75a483a7fa409ba6
Jannie87 <jannie.backman@gmail.com>
2022-05-30 17:03:32 +0200

    interface fix

--- client/src/InterFaces.ts
+++ client/src/InterFaces.ts
@@ -1,7 +1,18 @@
 export interface test {}
 
+// Vilket interface skall användas ?
+
+// export interface DeliveryInterface {
+//   name: string;
+//   price: number;
+//    time: number;
+// }
 export interface DeliveryInterface {
+  _id: string;
   name: string;
+  altText: string;
+  shippingTime: number;
   price: number;
-  time: number;
+  logo: string;
+  id: string;
 }
