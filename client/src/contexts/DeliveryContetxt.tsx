2795bb52c4c6d2bfb4938139037ea83e8aef41d7
Jannie87 <jannie.backman@gmail.com>
2022-05-30 16:54:57 +0200

    added context and provider.

--- client/src/contexts/DeliveryContetxt.tsx
+++ client/src/contexts/DeliveryContetxt.tsx
@@ -1,58 +1,110 @@
-import { time } from "console";
-import React, { createContext, FC, useContext, useState } from "react";
-import { number } from "yup";
-import { DeliveryFetch } from "../Api/Api";
-
-import { DeliveryInterface } from "../InterFaces";
+import React, {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import axios, { AxiosResponse } from "axios";
 
 interface DeliveryContextValue {
-  delivery?: DeliveryInterface;
   isLoading: boolean;
-  fetchDelivery: () => void;
+  deliveries: Delivery[];
+  fetchDeliveries: () => void;
+  fetchDelivery: (id: string) => void;
+  createDelivery: () => void;
+  updateDelivery: (delivery: Delivery) => void;
+  deleteDelivery: (id: string) => void;
 }
 
-// export interface Delivery {
-//   name: string;
-//   price: number;
-//   time: number;
-// }
+export interface Delivery {
+  _id: string;
+  name: string;
+  altText: string;
+  shippingTime: number;
+  price: number;
+  logo: string;
+  id: string;
+  time: number;
+}
 
-export const DeliveryContext = createContext<DeliveryContextValue>({
-  delivery: {
-    name: "",
-    price: 0,
-    time: 0,
-  },
+export const DeliveryContext = React.createContext<DeliveryContextValue>({
   isLoading: false,
-  fetchDelivery: () => {},
+  deliveries: [],
+  fetchDelivery: (id: string) => {},
+  fetchDeliveries: () => {},
+  createDelivery: () => {},
+  updateDelivery: (delivery: Delivery) => {},
+  deleteDelivery: (id: string) => {},
 });
 
-export const DeliveryProvider: React.FC<React.ReactNode> = ({children}) => {
-  const [delivery, setDelivery] = React.useState<DeliveryInterface>();
+export const DeliveryProvider: React.FC<React.ReactNode> = ({ children }) => {
+  const [deliveries, setDeliveries] = React.useState<Delivery[]>([]);
   const [isLoading, setIsLoading] = useState(false);
 
+  const fetchDeliveries = useCallback(() => {
+    axios.get<Delivery[]>("/api/delivery").then((res) => {
+      setDeliveries(res.data);
+    });
+  }, []);
 
-  const getDelivery = async (delivery: Promise<DeliveryInterface> => {
-    setIsLoading(true);
+  const fetchDelivery = useCallback((id: string) => {
+    axios.get<Delivery>(`/api/delivery/${id}`).then((res) => {
+      setDeliveries([res.data]);
+    });
+  }, []);
 
-    return DeliveryFetch(delivery)
-      .then((delivery) => {
-        setDelivery(delivery);
-        setIsLoading(false);
+  const createDelivery = useCallback(() => {
+    axios.post<Delivery>("http://localhost:5001/api/delivery").then((res) => {
+      setDeliveries([...deliveries, res.data]);
+    });
+  }, []);
 
-        // throw e;
+  const updateDelivery = useCallback((newDeliveryData: Delivery) => {
+    axios
+      .put<Delivery>(
+        `http://localhost:5001/api/deliveries/${newDeliveryData._id}`,
+        {
+          newDeliveryData,
+        }
+      )
+      .then((res) => {
+        const deliveryIndex = deliveries.findIndex((delivery: Delivery) => {
+          return (delivery._id = newDeliveryData._id);
+        });
+        deliveries[deliveryIndex] = res.data;
+        setDeliveries(deliveries);
       });
+  }, []);
+
+  const deleteDelivery = useCallback((id: string) => {
+    axios
+      .delete<Delivery>(`http://localhost:5001/api/delivery/${id}`)
+      .then((res) => {
+        const deliveryIndex = deliveries.findIndex((delivery: Delivery) => {
+          return (delivery._id = id);
+        });
+        setDeliveries([...deliveries.splice(deliveryIndex, 1)]);
+      });
+  }, []);
 
   return (
-    <DeliveryContext.Provider value={{ delivery }}>
+    <DeliveryContext.Provider
+      value={{
+        deliveries,
+        isLoading,
+        fetchDeliveries,
+        fetchDelivery,
+        createDelivery,
+        updateDelivery,
+        deleteDelivery,
+      }}
+    >
       {children}
     </DeliveryContext.Provider>
   );
 };
 
-export default DeliveryProvider;
-export const useDelivery = () => useContext(DeliveryContext);
-function setIsLoading(arg0: boolean) {
-  throw new Error("Function not implemented.");
-}
+export default DeliveryContext;
 
+export const useOrder = () => useContext(DeliveryContext);
