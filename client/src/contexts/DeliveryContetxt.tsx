dd7b975deb89c3a11faf724f60e9332499e423ec
Jannie87 <jannie.backman@gmail.com>
2022-05-30 12:55:44 +0200

    added bolean

--- client/src/contexts/DeliveryContetxt.tsx
+++ client/src/contexts/DeliveryContetxt.tsx
@@ -1,28 +1,58 @@
-import { createContext, FC, useContext, useState } from "react";
+import { time } from "console";
+import React, { createContext, FC, useContext, useState } from "react";
+import { number } from "yup";
+import { DeliveryFetch } from "../Api/Api";
 
 import { DeliveryInterface } from "../InterFaces";
 
-export interface ContextValue {
+interface DeliveryContextValue {
   delivery?: DeliveryInterface;
+  isLoading: boolean;
+  fetchDelivery: () => void;
 }
 
-export const DeliveryContext = createContext<ContextValue>({
+// export interface Delivery {
+//   name: string;
+//   price: number;
+//   time: number;
+// }
+
+export const DeliveryContext = createContext<DeliveryContextValue>({
   delivery: {
     name: "",
     price: 0,
     time: 0,
   },
+  isLoading: false,
+  fetchDelivery: () => {},
 });
 
-const DeliveryProvider: FC = (props) => {
-  const [delivery, setDelivery] = useState<DeliveryInterface>();
+export const DeliveryProvider: React.FC<React.ReactNode> = ({children}) => {
+  const [delivery, setDelivery] = React.useState<DeliveryInterface>();
+  const [isLoading, setIsLoading] = useState(false);
+
+
+  const getDelivery = async (delivery: Promise<DeliveryInterface> => {
+    setIsLoading(true);
+
+    return DeliveryFetch(delivery)
+      .then((delivery) => {
+        setDelivery(delivery);
+        setIsLoading(false);
+
+        // throw e;
+      });
 
   return (
     <DeliveryContext.Provider value={{ delivery }}>
-      {props.children}
+      {children}
     </DeliveryContext.Provider>
   );
 };
 
 export default DeliveryProvider;
 export const useDelivery = () => useContext(DeliveryContext);
+function setIsLoading(arg0: boolean) {
+  throw new Error("Function not implemented.");
+}
+
