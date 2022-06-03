89cb7cc20fb066a3d57af1b40791483aff1edcb2
ezzequ <esther.reichmann@me.com>
2022-06-03 11:50:59 +0200

    create account in client works but dont save to db

--- client/src/contexts/UserContext.tsx
+++ client/src/contexts/UserContext.tsx
@@ -1,18 +1,16 @@
-import React, {
-  useCallback,
-  useContext,
-} from "react";
+import React, { useCallback, useContext } from "react";
 import axios from "axios";
 
 interface UserContextValue {
+  isLoggedIn: boolean;
   users: User[];
   fetchUsers: () => void;
   fetchUser: (id: string) => void;
   createUser: () => void;
   updateUser: (user: User) => void;
   deleteUser: (id: string) => void;
-  loginUser: () => void
-  logoutUser:(id: string) => void
+  loginUser: () => void;
+  logoutUser: (id: string) => void;
 }
 
 /** Remember this is used backendside as well, update both of needed. */
@@ -21,24 +19,26 @@ export interface BaseUser {
   password: string;
 }
 
-export interface User extends BaseUser{
+export interface User extends BaseUser {
   _id: string;
   isAdmin: boolean;
 }
 
 export const UserContext = React.createContext<UserContextValue>({
   users: [],
-  fetchUsers: () => { },
-  fetchUser: (id: string) => { },
-  createUser: () => { },
-  updateUser: (user: User) => { },
-  deleteUser: (id: string) => { },
-  logoutUser: (id: string) => { },
-  loginUser: ()=> {}
+  fetchUsers: () => {},
+  fetchUser: (id: string) => {},
+  createUser: () => {},
+  updateUser: (user: User) => {},
+  deleteUser: (id: string) => {},
+  logoutUser: (id: string) => {},
+  loginUser: () => {},
+  isLoggedIn: false,
 });
 
 export const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
   const [users, setUsers] = React.useState<User[]>([]);
+  let [isLoggedIn, setIsLoggedIn] = React.useState(false);
 
   const fetchUsers = useCallback(() => {
     axios.get<User[]>("/api/user").then((res) => {
@@ -47,11 +47,9 @@ export const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
   }, []);
 
   const fetchUser = useCallback((id: string) => {
-    axios
-      .post<User>(`/api/user/${id}`)
-      .then((res) => {
-        setUsers([res.data]);
-      });
+    axios.post<User>(`/api/user/${id}`).then((res) => {
+      setUsers([res.data]);
+    });
   }, []);
 
   const createUser = useCallback(() => {
@@ -60,21 +58,18 @@ export const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
     });
   }, []);
 
-  const loginUser = useCallback(()=>{
+  const loginUser = useCallback(() => {
     axios.get<User>("/api/user", { withCredentials: true }).then((res) => {
       setUsers([...users, res.data]);
-      
     });
   }, []);
 
   const logoutUser = useCallback((id: string) => {
-    axios
-      .delete<User>(`/api/user/logout/${id}`)
-      .then((res) => {
-        const userIndex = users.findIndex((user: User) => {
-          return (user._id = id);
-        });
+    axios.delete<User>(`/api/user/logout/${id}`).then((res) => {
+      const userIndex = users.findIndex((user: User) => {
+        return (user._id = id);
       });
+    });
   }, []);
 
   const updateUser = useCallback((newUserData: User) => {
@@ -92,14 +87,12 @@ export const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
   }, []);
 
   const deleteUser = useCallback((id: string) => {
-    axios
-      .delete<User>(`/api/user/${id}`)
-      .then((res) => {
-        const userIndex = users.findIndex((user: User) => {
-          return (user._id = id);
-        });
-        setUsers([...users.splice(userIndex, 1)]);
+    axios.delete<User>(`/api/user/${id}`).then((res) => {
+      const userIndex = users.findIndex((user: User) => {
+        return (user._id = id);
       });
+      setUsers([...users.splice(userIndex, 1)]);
+    });
   }, []);
 
   return (
@@ -113,6 +106,7 @@ export const UserProvider: React.FC<React.ReactNode> = ({ children }) => {
         createUser,
         updateUser,
         deleteUser,
+        isLoggedIn,
       }}
     >
       {children}
